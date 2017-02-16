95d60fdef77889d2d4db421b0ee4cd0b8cea84f8
didierclin <didier.c620numericable.fr>
2017-02-16 13:51:49 +0100

    introduce REACT STATE notion, add initial sate to appointments component and initial values in form

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -4,8 +4,8 @@ var AppointmentForm = React.createClass({
       <div>
         <h2> Make a new Appointment </h2>
         <form>
-          <input name='title' placeholder='Appointment Title' />
-          <input name='apt_time' placeholder='Appointment date and time' />
+          <input name='title' placeholder='Appointment Title' value={this.props.input_title}/>
+          <input name='apt_time' placeholder='Appointment date and time' value={this.props.input_apt_time}/>
           <input type='submit' value='Make Appointment' />
         </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,20 @@
 var Appointments = React.createClass({
+  // single component above all the components that need the state in the hierarchy
+  // https://facebook.github.io/react/docs/thinking-in-react.html
+  getInitialState: function() {
+    return {
+      // return an object
+      appointments: this.props.appointments,
+      input_title: '',
+      input_apt_time: 'Tomorrow at 9am'
+    }
+  },
   render: function() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        {/* we now manage state */}
+        <AppointmentForm input_title={this.state.input_title} input_apt_time={this.state.input_apt_time} />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
      )
   }
